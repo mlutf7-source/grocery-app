9627e2caad30bf02e0a20d22f31dce9c680a8181
mlutf7-source <mlutf7@gmail.com>
2026-07-30 19:38:49 +0300

    Create usePreventLeave.ts

---
+++ src/hooks/usePreventLeave.ts
@@ -0,0 +1,18 @@
+import { useEffect } from 'react';
+
+export function usePreventLeave(hasChanges: boolean) {
+  useEffect(() => {
+    const handleBeforeUnload = (e: BeforeUnloadEvent) => {
+      if (hasChanges) {
+        e.preventDefault();
+        e.returnValue = '';
+      }
+    };
+
+    window.addEventListener('beforeunload', handleBeforeUnload);
+
+    return () => {
+      window.removeEventListener('beforeunload', handleBeforeUnload);
+    };
+  }, [hasChanges]);
+}
